6585de128b1f2d210f09734f0e398158868ecad9
FelixG <felix.gordillo@ucsp.edu.pe>
2024-12-05 19:08:47 -0500

    added connections and basic send msg

--- client.cpp
+++ client.cpp
@@ -1,10 +1,41 @@
 #include "Interface/TCPClient.h"
-
+#include "config.hpp"
 #include <iostream>
+#include <thread>
+#include <sstream> 
+#include <iomanip>
 using namespace std;
+
+TCPClient client(IP_ADDRESS, PORT);
+
+void readThread()
+{
+    string message = "";
+    while (true) {
+        message = client.receive(11);
+        if (message.empty()) {
+            cout << "message is empty" << endl;
+            break;
+        }
+        cout << "Received: " << message << endl;
+    }
+}
 
 int main()
 {
-    cout << "Hello World" << endl;
+    client.connectToServer();
+    thread(readThread).detach();
+    string message = "C";
+    client.send(message);
+    do  {
+        cout << "Enter message: ";
+        getline(cin, message);
+        if (message == "exit") {
+            client.send("o");
+            break;
+        }
+        client.send(message);
+    } while (true);
+
     return 0;
 }
--- processingServer.cpp
+++ processingServer.cpp
@@ -1,6 +1,24 @@
 #include "Interface/TCPClient.h"
+#include "config.hpp"
+#include <iostream>
+#include <thread>
+using namespace std;
+
+TCPClient processingServer(IP_ADDRESS, PORT);
 
 int main()
 {
+    processingServer.connectToServer();
+    string message = "P";
+    processingServer.send(message);
+    message = "";
+    while (true) {
+        cout << "Enter message: ";
+        getline(cin, message);
+        if (message == "exit") {
+            processingServer.send("o");
+            break;
+        }
+    }
     return 0;
 }
