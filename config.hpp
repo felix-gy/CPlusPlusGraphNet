0b2cdfc3f967183f1d9582e54bd434211db7496d
FelixG <felix.gordillo@ucsp.edu.pe>
2024-12-07 03:12:43 -0500

    update

--- config.hpp
+++ config.hpp
@@ -2,17 +2,20 @@
 #define CONFIG_HPP
 
 const char* const IP_ADDRESS = "127.0.0.1";
+//const char* const IP_ADDRESS = "4.201.139.31";
 const int PORT = 8080;
 
 // Matriz de Caracteristicas
-const char* const H_FILE = "Dataset/H_caracteristicas.txt";
+//const char* const H_FILE = "Dataset/H_caracteristicas.txt";
+const char* const H_FILE = "Dataset/FeatureMatrix_Counts_With_NodeID.csv";
 const int H_rows = 10;
-const int H_cols = 5;
+const int H_cols = 44;
 
 // Matriz de Pesos
-const char* const W_FILE = "Dataset/W_pesos.txt";
-const int W_rows = 5;
-const int W_cols = 18;
+//const char* const W_FILE = "Dataset/W_pesos.txt";
+const char* const W_FILE = "Dataset/weights_matrix2.csv";
+const int W_rows = 44;
+const int W_cols = 64;
 
 // Matriz resultante
 const char* const M_FILE = "Dataset/matrix_W_H.txt";
